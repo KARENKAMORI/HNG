0622f0b76c5c93dc25adad5950308172cdba6975
KARENKAMORI <kamorikaren2019@gmail.com>
2025-02-01 09:36:48 +0300

    First Task

---
+++ HNG_Task0/backend/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.utils.timezone import now
+
+def public_api(request):
+    response_data = {
+        "email": "kamorikaren2019@gmail.com",
+        "current_datetime": now().isoformat(),
+        "github_url": "https://github.com/yourusername/your-repo"
+    }
+    return JsonResponse(response_data)
+
