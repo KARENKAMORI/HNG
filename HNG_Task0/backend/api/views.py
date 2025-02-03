51f6991ea78a5bc04ed22c3f3acfaf99984a7f96
KARENKAMORI <kamorikaren2019@gmail.com>
2025-02-03 10:52:18 +0300

    changing the UTC time to be explicitly marked with a 'Z' at the end

--- HNG_Task0/backend/api/views.py
+++ HNG_Task0/backend/api/views.py
@@ -5,7 +5,7 @@ from django.utils.timezone import now
 def public_api(request):
     response_data = {
         "email": "kamorikaren2019@gmail.com",
-        "current_datetime": now().isoformat(),
+        "current_datetime": now().replace(microsecond=0).isoformat() + "Z",
         "github_url": "https://github.com/KARENKAMORI/HNG/tree/main/HNG_Task0/backend"
     }
     return JsonResponse(response_data)
